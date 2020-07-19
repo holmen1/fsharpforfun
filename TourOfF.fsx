5d4a8863836dd0869e3f94c537259cdb9eda48bb
holmen1 <holmen1@gmail.com>
2020-07-19 21:38:38 +0200

    Add lists

--- TourOfF.fsx
+++ TourOfF.fsx
@@ -31,3 +31,263 @@ module BasicFunctions =
 
     // This line uses '%f' to print the result as a float.  As with '%d' above, this is type-safe.
     printfn "The result of applying the 3rd sample function to (6.5 + 4.5) is %f" result3
+
+
+
+    module Immutability =
+
+    /// Binding a value to a name via 'let' makes it immutable.
+    ///
+    /// The second line of code fails to compile because 'number' is immutable and bound.
+    /// Re-defining 'number' to be a different value is not allowed in F#.
+    let number = 2
+    // let number = 3
+
+    /// A mutable binding.  This is required to be able to mutate the value of 'otherNumber'.
+    let mutable otherNumber = 2
+
+    printfn "'otherNumber' is %d" otherNumber
+
+    // When mutating a value, use '<-' to assign a new value.
+    //
+    // Note that '=' is not the same as this.  '=' is used to test equality.
+    otherNumber <- otherNumber + 1
+
+    printfn "'otherNumber' changed to be %d" otherNumber
+
+
+   module IntegersAndNumbers = 
+
+    /// This is a sample integer.
+    let sampleInteger = 176
+
+    /// This is a sample floating point number.
+    let sampleDouble = 4.1
+
+    /// This computed a new number by some arithmetic.  Numeric types are converted using
+    /// functions 'int', 'double' and so on.
+    let sampleInteger2 = (sampleInteger/4 + 5 - 7) * 4 + int sampleDouble
+
+    /// This is a list of the numbers from 0 to 99.
+    let sampleNumbers = [ 0 .. 99 ]
+
+    /// This is a list of all tuples containing all the numbers from 0 to 99 and their squares.
+    let sampleTableOfSquares = [ for i in 0 .. 99 -> (i, i*i) ]
+
+    // The next line prints a list that includes tuples, using '%A' for generic printing.
+    printfn "The table of squares from 0 to 99 is:\n%A" sampleTableOfSquares
+
+
+
+module Booleans =
+
+    /// Booleans values are 'true' and 'false'.
+    let boolean1 = true
+    let boolean2 = false
+
+    /// Operators on booleans are 'not', '&&' and '||'.
+    let boolean3 = not boolean1 && (boolean2 || false)
+
+    // This line uses '%b'to print a boolean value.  This is type-safe.
+    printfn "The expression 'not boolean1 && (boolean2 || false)' is %b" boolean3
+
+
+module StringManipulation = 
+
+    /// Strings use double quotes.
+    let string1 = "Hello"
+    let string2  = "world"
+
+    /// Strings can also use @ to create a verbatim string literal.
+    /// This will ignore escape characters such as '\', '\n', '\t', etc.
+    let string3 = @"C:\Program Files\"
+
+    /// String literals can also use triple-quotes.
+    let string4 = """The computer said "hello world" when I told it to!"""
+
+    /// String concatenation is normally done with the '+' operator.
+    let helloWorld = string1 + " " + string2 
+
+    // This line uses '%s' to print a string value.  This is type-safe.
+    printfn "%s" helloWorld
+
+    /// Substrings use the indexer notation.  This line extracts the first 7 characters as a substring.
+    /// Note that like many languages, Strings are zero-indexed in F#.
+    let substring = helloWorld.[0..6]
+    printfn "%s" substring
+
+
+
+module Tuples =
+
+    /// A simple tuple of integers.
+    let tuple1 = (1, 2, 3)
+
+    /// A function that swaps the order of two values in a tuple. 
+    ///
+    /// F# Type Inference will automatically generalize the function to have a generic type,
+    /// meaning that it will work with any type.
+    let swapElems (a, b) = (b, a)
+
+    printfn "The result of swapping (1, 2) is %A" (swapElems (1,2))
+
+
+module PipelinesAndComposition =
+
+    /// Squares a value.
+    let square x = x * x
+
+    /// Adds 1 to a value.
+    let addOne x = x + 1
+
+    /// Tests if an integer value is odd via modulo.
+    let isOdd x = x % 2 <> 0
+
+    /// A list of 5 numbers.  More on lists later.
+    let numbers = [ 1; 2; 3; 4; 5 ]
+
+    /// Given a list of integers, it filters out the even numbers,
+    /// squares the resulting odds, and adds 1 to the squared odds.
+    let squareOddValuesAndAddOne values = 
+        let odds = List.filter isOdd values
+        let squares = List.map square odds
+        let result = List.map addOne squares
+        result
+
+    printfn "processing %A through 'squareOddValuesAndAddOne' produces: %A" numbers (squareOddValuesAndAddOne numbers)
+    
+    /// A shorter way to write 'squareOddValuesAndAddOne' is to nest each
+    /// sub-result into the function calls themselves.
+    ///
+    /// This makes the function much shorter, but it's difficult to see the
+    /// order in which the data is processed.
+    let squareOddValuesAndAddOneNested values = 
+        List.map addOne (List.map square (List.filter isOdd values))
+
+    printfn "processing %A through 'squareOddValuesAndAddOneNested' produces: %A" numbers (squareOddValuesAndAddOneNested numbers)
+
+    /// A preferred way to write 'squareOddValuesAndAddOne' is to use F# pipe operators.
+    /// This allows you to avoid creating intermediate results, but is much more readable
+    /// than nesting function calls like 'squareOddValuesAndAddOneNested'
+    let squareOddValuesAndAddOnePipeline values =
+        values
+        |> List.filter isOdd
+        |> List.map square
+        |> List.map addOne
+
+    printfn "processing %A through 'squareOddValuesAndAddOnePipeline' produces: %A" numbers (squareOddValuesAndAddOnePipeline numbers)
+
+    /// You can shorten 'squareOddValuesAndAddOnePipeline' by moving the second `List.map` call
+    /// into the first, using a Lambda Function.
+    ///
+    /// Note that pipelines are also being used inside the lambda function.  F# pipe operators
+    /// can be used for single values as well.  This makes them very powerful for processing data.
+    let squareOddValuesAndAddOneShorterPipeline values =
+        values
+        |> List.filter isOdd
+        |> List.map(fun x -> x |> square |> addOne)
+
+    printfn "processing %A through 'squareOddValuesAndAddOneShorterPipeline' produces: %A" numbers (squareOddValuesAndAddOneShorterPipeline numbers)
+
+    /// A tuple consisting of an integer, a string,
+    /// and a double-precision floating point number.
+    let tuple2 = (1, "fred", 3.1415)
+
+    printfn "tuple1: %A\ttuple2: %A" tuple1 tuple2
+
+
+
+module Lists =
+
+    /// Lists are defined using [ ... ].  This is an empty list.
+    let list1 = [ ]  
+
+    /// This is a list with 3 elements.  ';' is used to separate elements on the same line.
+    let list2 = [ 1; 2; 3 ]
+
+    /// You can also separate elements by placing them on their own lines.
+    let list3 = [
+        1
+        2
+        3
+    ]
+
+    /// This is a list of integers from 1 to 1000
+    let numberList = [ 1 .. 1000 ]  
+
+    /// Lists can also be generated by computations. This is a list containing 
+    /// all the days of the year.
+    let daysList = 
+        [ for month in 1 .. 12 do
+              for day in 1 .. System.DateTime.DaysInMonth(2017, month) do 
+                  yield System.DateTime(2017, month, day) ]
+
+    // Print the first 5 elements of 'daysList' using 'List.take'.
+    printfn "The first 5 days of 2017 are: %A" (daysList |> List.take 5)
+
+    /// Computations can include conditionals.  This is a list containing the tuples
+    /// which are the coordinates of the black squares on a chess board.
+    let blackSquares = 
+        [ for i in 0 .. 7 do
+              for j in 0 .. 7 do 
+                  if (i+j) % 2 = 1 then 
+                      yield (i, j) ]
+
+    /// Lists can be transformed using 'List.map' and other functional programming combinators.
+    /// This definition produces a new list by squaring the numbers in numberList, using the pipeline 
+    /// operator to pass an argument to List.map.
+    let squares = 
+        numberList 
+        |> List.map (fun x -> x*x) 
+
+    /// There are many other list combinations. The following computes the sum of the squares of the 
+    /// numbers divisible by 3.
+    let sumOfSquares = 
+        numberList
+        |> List.filter (fun x -> x % 3 = 0)
+        |> List.sumBy (fun x -> x * x)
+
+    printfn "The sum of the squares of numbers up to 1000 that are divisible by 3 is: %d" sumOfSquares
+
+
+module Arrays =
+
+    /// This is The empty array.  Note that the syntax is similar to that of Lists, but uses `[| ... |]` instead.
+    let array1 = [| |]
+
+    /// Arrays are specified using the same range of constructs as lists.
+    let array2 = [| "hello"; "world"; "and"; "hello"; "world"; "again" |]
+
+    /// This is an array of numbers from 1 to 1000.
+    let array3 = [| 1 .. 1000 |]
+
+    /// This is an array containing only the words "hello" and "world".
+    let array4 = 
+        [| for word in array2 do
+               if word.Contains("l") then 
+                   yield word |]
+
+    /// This is an array initialized by index and containing the even numbers from 0 to 2000.
+    let evenNumbers = Array.init 1001 (fun n -> n * 2) 
+
+    /// Sub-arrays are extracted using slicing notation.
+    let evenNumbersSlice = evenNumbers.[0..500]
+
+    /// You can loop over arrays and lists using 'for' loops.
+    for word in array4 do 
+        printfn "word: %s" word
+
+    // You can modify the contents of an array element by using the left arrow assignment operator.
+    //
+    // To learn more about this operator, see: https://docs.microsoft.com/dotnet/fsharp/language-reference/values/index#mutable-variables
+    array2.[1] <- "WORLD!"
+
+    /// You can transform arrays using 'Array.map' and other functional programming operations.
+    /// The following calculates the sum of the lengths of the words that start with 'h'.
+    let sumOfLengthsOfWords = 
+        array2
+        |> Array.filter (fun x -> x.StartsWith "h")
+        |> Array.sumBy (fun x -> x.Length)
+
+    printfn "The sum of the lengths of the words in Array 2 is: %d" sumOfLengthsOfWords
+
